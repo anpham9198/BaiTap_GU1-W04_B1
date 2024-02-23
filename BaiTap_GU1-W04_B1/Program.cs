8188d9d7de40b20e7574b53ec40ed904ddb85128
anpham9198 <158065764+anpham9198@users.noreply.github.com>
2024-02-23 09:50:13 +0700

    Initial commit

---
+++ BaiTap_GU1-W04_B1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap_GU1_W04_B1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 6, 7, 8, 9 };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.Write("Tong cac phan tu = " + sum);
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
